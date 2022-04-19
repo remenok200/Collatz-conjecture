93dd0b4045324e69100b90391c85030d9e7330ab
Roman Ratushnyi <remenok9@gmail.com>
2022-04-20 01:39:24 +0300

    Add JS calculate

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -2,3 +2,4 @@ const goBtn = document.querySelector('.getValue > button');
 const clearBtn = document.querySelector('.getValue > .reset');
 const number = document.querySelector('.number');
 const form = document.querySelector('.getValue');
+const main = document.querySelector('main');
--- assets/js/utils/calculate.js
+++ assets/js/utils/calculate.js
@@ -12,15 +12,85 @@ function calculate(e) {
       throw new RangeError("Input number must be a integer!");
     }
 
+    form.append(createElement("button", { classNames: ["reset"] }, "Clear."));
+    main.append(
+      createElement(
+        "table",
+        { classNames: ["main-table"] },
+        createElement(
+          "caption",
+          { classNames: ["main-table-caption"] },
+          "Calculations"
+        ),
+        createElement(
+          "tbody",
+          { classNames: ["main-table-contents"] },
+          createElement(
+            "tr",
+            {},
+            createElement("td", {}, "Iteration number"),
+            createElement("td", {}, "Contents of the calculation"),
+            createElement("td", {}, "Result"),
+            createElement("td", {}, "Picture")
+          )
+        )
+      )
+    );
+    const table = document.querySelector(".main-table");
+
     let counter = 0;
     while (numberFromForm !== 1) {
       if (numberFromForm % 2 === 0) {
+        const temp = numberFromForm;
+
         numberFromForm /= 2;
         counter++;
+
+        table.append(
+          createElement(
+            "tr",
+            {},
+            createElement("th", {}, counter),
+            createElement("th", {}, `${temp} / 2 =`),
+            createElement("th", {}, `${numberFromForm}`),
+            createElement(
+              "th",
+              {},
+              createElement("img", {
+                attributes: {
+                  src: "./assets/icons/even.svg",
+                  alt: "even result",
+                },
+              })
+            )
+          )
+        );
         console.log(numberFromForm, counter);
       } else {
-        numberFromForm = (numberFromForm * 3) + 1;
+        const temp = numberFromForm;
+
+        numberFromForm = numberFromForm * 3 + 1;
         counter++;
+
+        table.append(
+          createElement(
+            "tr",
+            {},
+            createElement("th", {}, counter),
+            createElement("th", {}, `${temp} * 3 + 1 =`),
+            createElement("th", {}, `${numberFromForm}`),
+            createElement(
+              "th",
+              {},
+              createElement("img", {
+                attributes: {
+                  src: "./assets/icons/odd.svg",
+                  alt: "odd result",
+                },
+              })
+            )
+          )
+        );
         console.log(numberFromForm, counter);
       }
     }
