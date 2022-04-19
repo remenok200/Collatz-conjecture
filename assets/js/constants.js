31fd6248686a87c8bd61d6076baa91b590e9d111
Roman Ratushnyi <remenok9@gmail.com>
2022-04-20 00:47:52 +0300

    Added JS calculate layout (without DOM)

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,1 +1,4 @@
 const goBtn = document.querySelector('.getValue > button');
+const clearBtn = document.querySelector('.getValue > .reset');
+const number = document.querySelector('.number');
+const form = document.querySelector('.getValue');
--- assets/js/utils/calculate.js
+++ assets/js/utils/calculate.js
@@ -1,5 +1,30 @@
 function calculate(e) {
-    e.preventDefault();
-    
-    console.log(1);
+  e.preventDefault();
+
+  let numberFromForm = Number(number.value);
+  try {
+    if (typeof numberFromForm !== "number") {
+      form.reset();
+      throw new TypeError("Input number must be a number!");
+    }
+    if (!Number.isInteger(numberFromForm)) {
+      form.reset();
+      throw new RangeError("Input number must be a integer!");
+    }
+
+    let counter = 0;
+    while (numberFromForm !== 1) {
+      if (numberFromForm % 2 === 0) {
+        numberFromForm /= 2;
+        counter++;
+        console.log(numberFromForm, counter);
+      } else {
+        numberFromForm = (numberFromForm * 3) + 1;
+        counter++;
+        console.log(numberFromForm, counter);
+      }
+    }
+  } catch (e) {
+    throw new Error(e);
+  }
 }
